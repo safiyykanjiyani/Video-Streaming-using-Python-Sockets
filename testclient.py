1a5cb781b22ddb7fbdea3b4d85d28de5116cdd43
safiyykanjiyani <safiyykanjiyani@gmail.com>
2021-05-09 22:15:28 -0700

    added comments (style!!)

--- testclient.py
+++ testclient.py
@@ -6,38 +6,58 @@ import struct
 #address = input("What is server address? ")
 #port = int(input("What is server port? "))
 
+#create a socket
 socket = socket(AF_INET, SOCK_STREAM)
+#choose server IP and port
 address = '192.168.56.1'
 port = 9001
+#try to connect to server
 socket.connect((address, port))
 
+#data will be what we gather from our recv's
 data = bytearray()
 
-
+#keep looping
 while True:
+    #when we start a frame
     while len(data) < 4:
+        #get the header
         packet = socket.recv(4)
+        #if no header and data, server is done transmitting
         if not packet:
             break
+        #add the packet to our data
         data.extend(packet)
+    #read the header
     length = struct.unpack(">I", data[:4])[0]
+    #data now excludes header, so we can parse the frame
     data = data[4:]
 
+    #while we don't have the complete frame
     while len(data) < length:
+        #keep recv
         packet = socket.recv(4096)
         #print("processing", len(data))
+        #and add it to our data
         data.extend(packet)
+    #parse our frame from our data
     compressed = data[:length]
+    #the rest is for next frame
     data  = data[length:]
+    #get our frame from bytestream
     frame = pickle.loads(compressed)
+    #display the frame on the UI
     cv2.imshow("Client",frame)
+    #every milisecond, check for user key input
     key = cv2.waitKey(1)
+    #if the key is Q, then quit the program
     if key == ord('q'):
         break
+    #if the key is a space, then pause the film
     while key in [ord(' ')]:
         key = cv2.waitKey(0)
         if key == ord(' '):
             break
-# Quit when 'q' is pressed
+    #quit when 'q' is pressed
     if key == ord('q'):
         break
--- testserver.py
+++ testserver.py
@@ -3,33 +3,40 @@ import cv2
 import pickle
 import struct
 
-
+#create a TCP socket
 socket = socket(AF_INET, SOCK_STREAM)
+#set address and port
 address = '192.168.56.1'
 port = 9001
+#bind to socket and start listening
 socket.bind((address,port))
 socket.listen(1)
-videofile = "bird.avi"
+#choose videofile to display!
+videofile = "fountain.mp4"
 
 while True:
+    #keep looking for clients
     client, addr = socket.accept()
+    #if client is present
     if client:
+        #open up the video
         video = cv2.VideoCapture(videofile)
         while video.isOpened():
+            #grab the frame
             ret, frame = video.read()
-            cv2.imshow("Server", frame)
+            #turn frame into a byte stream
             compressed = pickle.dumps(frame)
+            #make a packet whose header has size (in bytes) of the frame
             packet = struct.pack('>I', len(compressed)) + compressed
-            #length = struct.unpack('>I', packet[:4])[0]
-            #print("frame length:", length)
-            #compressed = packet[4:]
-            #frame = pickle.loads(compressed)
+
+            '''
+            length = struct.unpack('>I', packet[:4])[0]
+            print("frame length:", length)
+            compressed = packet[4:]
+            frame = pickle.loads(compressed)
+            '''
+            #send the packet to the client
             client.sendall(packet)
             #cv2.waitKey(1)
             #cv2.imshow("Server", frame)
-            if not ret:
-                print("last frame")
-                break
-        video.release()
-        cv2.destroyAllWindows()
     socket.close()
